bab3702d3a801b2e6d9b8c5cecadd7094983e746
John Hayes-Reed <john.hayes.reed@gmail.com>
2016-10-22 23:39:43 +0900

    Change production default url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'secret-badlands-56956.herokuapp.com' }
 end
